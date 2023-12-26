62fd47c2941a0ce4da53ea4bbdb1e2eec849db66
송동철 <charles@songdongcheol-ui-macbookair.local>
2023-12-26 19:14:38 +0900

    # UI만들기 시작  #   design      : 로그인 화면 구성 UI 제작  # 어떤 화면인지 알리는 텍스트 추가  # 로고로 쓰일 이미지와 마켓이름 텍스트 추가  # 아이디 비밀번호 입력란 뒤에 눈에 띄게 LinearLayout에 BackGround 이용  # EditText입력란에 Hint로 입력란 구분  # 화면 이동을 위한 로그인과 회원가입 버튼 추가

--- app/src/main/java/com/example/spartamarket/MainActivity.kt
+++ app/src/main/java/com/example/spartamarket/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.spartamarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_main)
+        btn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
+
 }
