5b16c047313d9873dfa5b1031983749d59fdc9f5
Ohleesang <jammmmed@gmail.com>
2023-12-26 17:02:49 +0900

    Style : MypageActivity의 UI 작성 Wireframing 참고하여 UI를 작성 @ 대략적인 기능별로 layout을 나눔   - layout_user : 로그인한 회원의 이름,이메일를 출력   - layout_cart : 장바구니 list를 보여줌   - layout_order : 구입한 상품 list를 보여줌
    - ImageView : 나중에 복합적인 Widget 으로 대체 예정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.spartamarket"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.spartamarket"
