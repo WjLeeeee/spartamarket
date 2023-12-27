4e4ae4031ed631519f4bfde369c01d705b7bfccf
Ohleesang <jammmmed@gmail.com>
2023-12-27 17:35:44 +0900

    cardView에 출력될 해당 객체들의 리스트를 선언

--- app/src/main/java/com/example/spartamarket/MypageActivity.kt
+++ app/src/main/java/com/example/spartamarket/MypageActivity.kt
@@ -6,14 +6,24 @@ import android.os.Bundle
 import android.widget.Button
 
 class MypageActivity : AppCompatActivity() {
+
+    private lateinit var btnLogout :Button
+    private lateinit var btnBuy :Button
+
+    private var buyList = mutableListOf<Product?>()
+    private var basketList = mutableListOf<Product?>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
-        val btnLogout = findViewById<Button>(R.id.btn_logout)
+        //button id 연결
+        btnLogout = findViewById<Button>(R.id.btn_logout)
 
 
         btnLogout.setOnClickListener {
             finish()
         }
+
+        //intent 수신
     }
 }
