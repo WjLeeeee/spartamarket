c0d504c8fefd434cb46987836f438b6c45b044dc
WjLeeeee <dldnwls0115@naver.com>
2023-12-27 15:28:36 +0900

    scope function사용해서 putextra해줄때 apply사용해서 가동석 높임

--- app/src/main/java/com/example/spartamarket/MainActivity.kt
+++ app/src/main/java/com/example/spartamarket/MainActivity.kt
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ScrollView
-import android.widget.Toast
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
-    private val loginBtn : Button by lazy{findViewById(R.id.login_btn)}
-    private val mypageBtn : Button by lazy{findViewById(R.id.mypage_btn)}
+    private val loginBtn: Button by lazy { findViewById(R.id.login_btn) }
+    private val mypageBtn: Button by lazy { findViewById(R.id.mypage_btn) }
 
 
     private lateinit var scrollViewMac: ScrollView
@@ -44,33 +42,43 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun productChange(view: View){
-        when(view.id){
-            R.id.img_mac-> {
-                scrollViewWindow.isVisible=false
-                scrollViewMac.isVisible=true
+    fun productChange(view: View) {
+        when (view.id) {
+            R.id.img_mac -> {
+                scrollViewWindow.isVisible = false
+                scrollViewMac.isVisible = true
             }
+
             R.id.img_window -> {
-                scrollViewWindow.isVisible=true
-                scrollViewMac.isVisible=false
+                scrollViewWindow.isVisible = true
+                scrollViewMac.isVisible = false
             }
         }
     }
 
-    fun productWindowClicked(view:View){
+    fun productWindowClicked(view: View) {
         val index = productWindow.indexOfChild(view)
         val selectedProduct = Product.productWindowList[index]
 
-        val intent = Intent(this, DetailActivity::class.java)
-        intent.putExtra("selectedProduct", selectedProduct)
+        val intent = Intent(this, DetailActivity::class.java).apply {
+            putExtra(
+                "selectedProduct",
+                selectedProduct
+            )
+        }
         startActivity(intent)
     }
-    fun productMacClicked(view:View){
+
+    fun productMacClicked(view: View) {
         val index = productMac.indexOfChild(view)
         val selectedProduct = Product.productMacList[index]
 
-        val intent = Intent(this, DetailActivity::class.java)
-        intent.putExtra("selectedProduct", selectedProduct)
+        val intent = Intent(this, DetailActivity::class.java).apply {
+            putExtra(
+                "selectedProduct",
+                selectedProduct
+            )
+        }
         startActivity(intent)
     }
 }
