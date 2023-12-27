40533e5ad26a099ecfb93f6f11d3c315715130fa
Ohleesang <jammmmed@gmail.com>
2023-12-27 13:02:59 +0900

    로그아웃 버튼 클릭시 액티비티 종료

--- app/src/main/java/com/example/spartamarket/MypageActivity.kt
+++ app/src/main/java/com/example/spartamarket/MypageActivity.kt
@@ -1,11 +1,19 @@
 package com.example.spartamarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+        val btnLogout = findViewById<Button>(R.id.btn_logout)
+
+
+        btnLogout.setOnClickListener {
+            finish()
+        }
     }
 }
