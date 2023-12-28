839607e99cc3c4033417e1557032e1402e741525
Ohleesang <jammmmed@gmail.com>
2023-12-28 11:53:44 +0900

    viewOrderList() : 장바구니 목록 리스트를 view를 생성하여 보여줌

--- app/src/main/java/com/example/spartamarket/MypageActivity.kt
+++ app/src/main/java/com/example/spartamarket/MypageActivity.kt
@@ -30,7 +30,7 @@ class MypageActivity : AppCompatActivity() {
 
 
         viewCartList()
-
+        viewOrderList()
 
     }
     fun viewCartList(){
@@ -58,5 +58,30 @@ class MypageActivity : AppCompatActivity() {
             layoutCart.addView(cardView)
         }
 
+    }
+    fun viewOrderList(){
+        //객체에 따른 레이아웃에 CardView 값 추가
+        var layoutCart = findViewById<LinearLayout>(R.id.order_layout)
+
+        //layout_card의 xml 속성값을 copy 하고싶다.
+        var layoutCard = findViewById<ConstraintLayout>(R.id.layout_card_horizontal)
+
+        MainActivity.list.buyList.forEach {
+            var cardView = CardView(this)
+            var constraintLayout = ConstraintLayout(this)
+            val imageView = ImageView(this)
+            val textView = TextView(this)
+
+            //속성 정리
+            constraintLayout.layoutParams = layoutCard.layoutParams
+            imageView.setImageResource(it?.imageResId ?: 0)
+            textView.setText(it?.name)
+
+            cardView.addView(constraintLayout)
+            constraintLayout.addView(imageView)
+            constraintLayout.addView(textView)
+
+            layoutCart.addView(cardView)
+        }
     }
 }
